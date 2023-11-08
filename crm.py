a096f1d36f98f808830e84cee08497eccff37c54
Maxime <maxilefranc@gmail.com>
2023-11-08 10:42:18 -0500

    Class User, DB connection, and firsts tests DONE

---
+++ crm.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import ClassVar
+from pathlib import Path
+import re
+import string
+
+from tinydb import TinyDB, where, table
+
+
+@dataclass
+class User:
+    first_name: str
+    last_name: str
+    phone_number: str = ''
+    address: str = ''
+
+    DB: ClassVar = TinyDB(Path(__file__).resolve().parent / 'db.json', indent=4)
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    @property
+    def db_instance(self) -> table.Document | list[table.Document] | None:
+        return User.DB.get((where('first_name') == self.first_name) & (where('last_name') == self.last_name))
+
+    def __str__(self) -> str:
+        return f'{self.full_name}\n{self.phone_number}\n{self.address}'
+
+    def _checks(self):
+        self._check_phone_number()
+        self._check_names()
+
+    def _check_phone_number(self):
+        phone_number = re.sub(r"^\+33|[()\s*]", "", self.phone_number)
+
+        if len(phone_number) == 9 and not phone_number.startswith('0'):
+            phone_number = '0' + phone_number
+
+        if len(phone_number) < 10 or not phone_number.isdigit():
+            raise ValueError(f'Numéro de téléphone {phone_number} invalide.')
+
+    def _check_names(self):
+        if not (self.first_name and self.last_name):
+            raise ValueError('Le prénom et le nom de famille ne peuvent pas être vides.')
+
+        special_characters = string.punctuation + string.digits
+        for character in self.full_name.strip():
+            if character in special_characters:
+                raise ValueError(f'Nom "{self.full_name}" invalide')
+
+    def exists(self) -> bool:
+        return bool(self.db_instance)
+
+    def delete(self) -> list[int]:
+        if self.exists():
+            return User.DB.remove(doc_ids=[self.db_instance.doc_id])  # type: ignore
+        return []
+
+    def save(self, validate_data: bool = False) -> int:
+        if validate_data:
+            self._checks()
+
+        if self.exists():
+            return -1
+        else:
+            return User.DB.insert(self.__dict__)
+
+
+def get_all_users():
+    return [User(**user) for user in User.DB.all()]
+
+
+if __name__ == '__main__':
+    adrien = User('Adrien', 'Klein')
+    print(adrien.delete())
---
+++ test_crm.py
@@ -0,0 +1,98 @@
+import pytest
+from tinydb import TinyDB, table
+from tinydb.storages import MemoryStorage
+
+
+from crm import User
+
+
+@pytest.fixture
+def setup_db():
+    User.DB = TinyDB(storage=MemoryStorage)
+
+
+@pytest.fixture
+def user(setup_db):
+    u = User('Patrick', 'Martin', '0668500036', '1 rue du chemin, 75000 Paris')
+    u.save()
+    return u
+
+
+def test_first_name(user):
+    assert user.first_name == 'Patrick'
+
+
+def test_full_name(user):
+    assert user.full_name == 'Patrick Martin'
+
+
+def test_db_instance(user):
+    assert isinstance(user.db_instance, table.Document)
+    assert user.db_instance['first_name'] == 'Patrick'
+    assert user.db_instance['last_name'] == 'Martin'
+    assert user.db_instance['phone_number'] == '0668500036'
+    assert user.db_instance['address'] == '1 rue du chemin, 75000 Paris'
+
+
+def test_not_db_instance(setup_db):
+    u = User('Maxime', 'Lefranc',  '0123456443', '18 rue Sabaneta, 34000 Montpellier')
+    assert u.db_instance is None
+
+
+def test_check_phone_number(setup_db):
+    user_good = User('Jean', 'Smith', '0123456789', '1 rue du chemin, 75015, Paris')
+    user_bad = User('Jean', 'Smith', 'abcd', '1 rue du chemin, 75015, Paris')
+
+    with pytest.raises(ValueError) as err:
+        user_bad._check_phone_number()
+
+    assert str(err.value) == 'Numéro de téléphone abcd invalide.'
+
+    user_good.save(validate_data=True)
+    assert user_good.exists() is True
+
+
+def test_check_names_empty(setup_db):
+    user_bad = User('', '', '0123456789', '1 rue du chemin, 75015, Paris')
+    with pytest.raises(ValueError) as err:
+        user_bad._check_names()
+
+    assert str(err.value) == 'Le prénom et le nom de famille ne peuvent pas être vides.'
+
+
+def test_check_names_invalid_characters(setup_db):
+    user_bad = User('Patrick#@&', '#@&$$', '0123456789', '1 rue du chemin, 75015, Paris')
+    with pytest.raises(ValueError) as err:
+        user_bad._check_names()
+
+    assert str(err.value) == 'Nom "Patrick#@& #@&$$" invalide'
+
+
+def test_exists(user):
+    assert user.exists() is True
+
+
+def test_not_exists(setup_db):
+    u = User('Maxime', 'Lefranc',  '0123456443', '18 rue Sabaneta, 34000 Montpellier')
+    assert u.exists() is False
+
+
+def test_delete(user):
+    user.save()
+    first = user.delete()
+    second = user.delete()
+    assert len(first) > 0
+    assert isinstance(first, list)
+    assert len(second) == 0
+    assert isinstance(second, list)
+
+
+def test_save(setup_db):
+    user_test = User('John', 'Smith', '1 rue du chemin, 75015, Paris')
+    user_test_dup = User('John', 'Smith', '1 rue du chemin, 75015, Paris')
+    first = user_test.save()
+    second = user_test_dup.save()
+    assert isinstance(first, int)
+    assert isinstance(second, int)
+    assert first > 0
+    assert second == -1
